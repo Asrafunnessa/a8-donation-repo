696ae8493744d2b63f3630400e87b690324ad381
Asrafunnessa <ayeshapriya0@gmail.com>
2023-09-25 09:44:20 +0600

    Set error page

--- src/Pages/EachCategory/EachCategory.jsx
+++ src/Pages/EachCategory/EachCategory.jsx
@@ -1,6 +1,9 @@
+// import { useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
 
 const EachCategory = () => {
+
+    // const [EachCategory, setEachCategory] = useState();
 
     const params = useParams()
     console.log(params);
